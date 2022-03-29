184b14e119ce3b29104f7506abf1ef7bbcf36bdf
张志阵 <1809642355@qq.com>
2022-03-29 17:50:38 +0800

    feat:完善信息；添加MIT协议

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Description: File Description
  * @FilePath: /study/temperMonkey/splunk_search/README.md
  * @LastEditors: zzz
- * @LastEditTime: 2022-03-29 16:19:55
+ * @LastEditTime: 2022-03-29 17:50:16
 -->
 ### **`temperMonkey脚本`** splunk 快速搜索 
 
@@ -16,3 +16,7 @@
 
 ## Windows
 通过键入 `Alt + r` 来实现 快速搜索
+
+### 入口
+* [Greasy Fork](https://greasyfork.org/zh-CN/scripts/442300-splunk-search)
+* [Gist](https://gist.github.com/zzailianlian/0466db69a7cb663aea32a467c7be8f34)
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         splunk快速搜索
+// @name         splunk search
+// @name:zh-CN   splunk快速搜索
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  通过键入 `option + r` 来实现splunk快速搜索!
-// @author       You
-// @match        http://splunk.ali.plt.babytree-inc.com/*
+// @author       https://github.com/zzailianlian
+// @license      MIT
+// @match        http://splunk.ali.plt.*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=babytree-inc.com
 // @grant        none
 // ==/UserScript==
@@ -15,18 +17,19 @@
   document.onkeydown = function (event) {
       var e = event || window.event;
       console.log(e, e.keyCode);
-       if (e && e.altKey && e.keyCode == 82) { // 按 Mac 的 option + r
+       if (e && e.altKey && e.keyCode == 82) { 
+         // 按 Mac 的 option + r 或者 Windows 的Alt + r
          // 重新搜索splunk
          document.querySelector("body > div.shared-page > div.main-section-body > div > div.section-padded.section-header > div.search-bar-wrapper.shared-searchbar > form > table > tbody > tr > td.search-button > a").click()
        }
       if (e && e.keyCode == 27) { // 按 Esc
-          //要做的事情
+          //...
       }
       if (e && e.keyCode == 113) { // 按 F2
-          //要做的事情
+          //...
       }
       if (e && e.keyCode == 13) { // enter 键
-          //要做的事情
+          //...
       }
   };
 
